8ff57a33fa549a83958a17c99a00806279d3996e
Michael Weedman <michael.weedman@gmail.com>
2020-01-31 11:48:43 -0700

    Deployed server. Trying to connect.

--- src/Components/Add-Animal/AddAnimalForm.js
+++ src/Components/Add-Animal/AddAnimalForm.js
@@ -5,14 +5,27 @@ class Add extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      petInfo: {}
     }
+  }
+
+  handleSubmit = () => {
+
   }
 
   render() {
     return(
       <div>
         Pets Here! Add Your Pets here!
+        <form className="submissionForm" onSubmit={this.handleSubmit}> 
+          <select onChange={this.handleSelect}>Animal Type</select>
+          <label for='animalName'>Name</label>
+          <input id='animalName' value={this.setName} placeholder="Name" required></input>
+          <input id='animalSex' value={this.setSex} placeholder="Sex" required></input>
+          <input required id='animalBreed' placeholder='Breed'></input>
+          <input required id='animalColor' placeholder='Color'></input>
+          <button type='Submit'>Place Pet for Adoption</button>
+        </form>
       </div>
     )
   }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 
 export default {
-  REACT_APP_API_ADDRESS: process.env.REACT_APP_API_ADDRESS || 'http://localhost:8000/api'
+  REACT_APP_API_ADDRESS: process.env.REACT_APP_API_ADDRESS || 'https://michael-angel-petful-server.herokuapp.com/'
 }
